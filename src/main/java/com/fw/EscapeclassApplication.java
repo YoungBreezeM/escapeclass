6c1767451794d81e9acb9418b80b5c7a5de4f943
yqffqy <940695836@qq.com>
2020-04-11 15:37:39 +0800

    Initial commit

---
+++ src/main/java/com/fw/EscapeclassApplication.java
@@ -0,0 +1,13 @@
+package com.fw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscapeclassApplication.class, args);
+    }
+
+}
